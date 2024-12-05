8663833c4e86646b30677f194a7a9f59f3f9a467
gunelbalayeva <gunelbalayeva97@gmail.com>
2024-12-05 18:24:45 +0400

    Initial Commit

---
+++ newRepositoryDemo/newRepositoryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  newRepositoryDemoApp.swift
+//  newRepositoryDemo
+//
+//  Created by User on 05.12.24.
+//
+
+import SwiftUI
+
+@main
+struct newRepositoryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
